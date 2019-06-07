99f4e066136e0ad34235915948f1a4c34eb6f1c0
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-06-07 09:03:34 -0500

    mostrando data

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,36 +1,92 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import List from './List';
 
-class Search extends Component {
- state = {
-   query: '',
- }
+class Character extends Component {
+  constructor(props){
+    super(props);
 
- handleInputChange = () => {
-   this.setState({
-     query: this.search.value
-   })
- }
+    this.state = {
+      people: [], 
+    }
 
- render() {
-   return (
-    <div>
-      <div className="row">
-        <h1>STAR WARS</h1>
+    this.getPeople = this.getPeople.bind(this); 
+  }
+
+  getPeople(){
+    return axios.get("https://swapi.co/api/people/")
+    .then((response) => {
+      console.log(response.data.results); 
+      this.setState( { people: response.data.results } )
+    })
+  }
+
+  componentDidMount(){
+    this.getPeople()
+  }
+
+  render() {
+    const {people} = this.state; 
+    return (
+      <div className="App">
+        <List people={people} />
       </div>
-      <div className="row">
-        <h2>PERSONAJES</h2>
-      </div>
-      <form>
-        <input
-            placeholder="BUSCAR"
-            ref={input => this.search = input}
-            onChange={this.handleInputChange}
-        />
-        <p>{this.state.query}</p>
-      </form>
-    </div>
-   )
- }
+    );
+  }
+//  state = {
+//    query: '',
+//  }
+
+//  getInfo = () => {
+//   return axios.get("http://swapi.co/api/people")
+//   .then((response) => {
+//     console.log(response.data.results); 
+//     this.setState( { results: response.data.results } )
+//   })
+// }
+
+// //  getInfo = () => {
+// //   axios.get('http://swapi.co/api/people/1')
+// //     .then(({ data }) => {
+// //       this.setState({
+// //         results: data.data // MusicGraph returns an object named data, 
+// //                            // as does axios. So... data.data                             
+// //       })
+// //     })
+// // }
+
+// handleInputChange = () => {
+//   this.setState({
+//     query: this.search.value
+//   }, () => {
+//     if (this.state.query && this.state.query.length > 1) {
+//       if (this.state.query.length % 2 === 0) {
+//         this.getInfo()
+//       }
+//     } 
+//   })
+// }
+
+//  render() {
+//    return (
+//     <div>
+      // <div className="row">
+      //   <h1>STAR WARS</h1>
+      // </div>
+      // <div className="row">
+      //   <h2>PERSONAJES</h2>
+      // </div>
+      // <form>
+      //   <input
+      //       placeholder="BUSCAR"
+      //       ref={input => this.search = input}
+      //       onChange={this.handleInputChange}
+      //   />
+      //   <p>{this.state.query}</p>
+      // </form>
+//     </div>
+//    )
+//  }
 }
 
-export default Search
+export default Character
---
+++ src/components/List.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import CharInfo from './CharInfo'; 
+
+class List extends Component {
+  render() {
+    const people = this.props.people; 
+
+    return (
+      <div>
+        <div className="row">
+          <h1>STAR WARS</h1>
+        </div>
+        <div className="row">
+          <h2>PERSONAJES</h2>
+        </div>
+        {/* <form>
+          <input
+            placeholder="BUSCAR"
+            ref={input => this.search = input}
+            onChange={this.handleInputChange}
+          />
+          <p>{this.state.query}</p>
+        </form> */}
+        {
+        people.map((p) => {
+          console.log(p)
+            return (
+              <div key={p.url}>
+                <h1 className="char-name">{p.name}</h1> 
+                <CharInfo charInfo={p} /> 
+              </div>
+            )
+          })
+        }
+      </div>
+      );
+  }
+}
+
+export default List; 
