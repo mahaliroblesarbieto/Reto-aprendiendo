a9a0f07de9671a13b1051108f39526c92b114a8a
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-06-08 16:43:36 -0500

    se muestra nombre en componente List

--- src/components/List.js
+++ src/components/List.js
@@ -4,14 +4,18 @@ import './styles/Character.css';
  
 
 function List (props) {
+  console.log(props.location.state.info)
     return ( 
       <div>
-        <ul>
-          <li><h2>Peso:{props.location.state.info.mass}</h2></li> 
-            <li><h2>Año:{props.location.state.info.birth_year}</h2></li> 
-            <li><h2>Género:{props.location.state.info.gender}</h2></li> 
-        </ul>
+        <h1>{props.location.state.info.name}</h1>
+        <div>
+          <h2>Peso:{props.location.state.info.mass}</h2>
+          <h2>Año:{props.location.state.info.birth_year}</h2>
+          <h2>Género:{props.location.state.info.gender}</h2> 
+        </div>
+        <div>
         <Link to='/'><p className="btn btn-danger">Volver</p></Link>
+        </div>
       </div>
       );
   }
