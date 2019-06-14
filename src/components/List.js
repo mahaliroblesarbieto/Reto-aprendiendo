ff38141cc4641a59b408f4e26afcb27606c098da
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-06-14 10:12:32 -0500

    el anterior commit era año este es peso se crea la misma funcionalidad para peso

--- src/components/List.js
+++ src/components/List.js
@@ -9,7 +9,7 @@ function List (props) {
       <div className="margin-h1">
         <h1 className="size-h1 margin-h1">{props.location.state.info.name}</h1>
         <div className="margin-info">
-          <p>Peso: {props.location.state.info.mass}</p>
+          <p>Peso: {props.location.state.info.mass === 'unknown' ? 'Desconocido' : props.location.state.info.mass}</p>
           <p>Año: {props.location.state.info.birth_year === 'unknown' ? 'Desconocido' : props.location.state.info.birth_year}</p>
           <p>Género: {props.location.state.info.gender}</p> 
         </div>
